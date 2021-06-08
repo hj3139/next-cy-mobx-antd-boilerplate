c29231c70cbd86b15a05c270f639ede25d120dd9
imform-mm-2112 <hojung@imform-mm-2112ui-Macmini.local>
2021-06-08 17:35:18 +0900

    first

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 const Home = () => {
-  const a = 'a';
   return (
     <div className={styles.container}>
       <Head>
